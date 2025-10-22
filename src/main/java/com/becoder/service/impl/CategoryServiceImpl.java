cf2ece83265e9a8ab07cf6cdffffdd6879fced5f
asha <ashasonkamble24@gmail.com>
2025-10-22 17:15:20 +0530

    update category implementation

--- src/main/java/com/becoder/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/becoder/service/impl/CategoryServiceImpl.java
@@ -37,9 +37,17 @@ public class CategoryServiceImpl implements CategoryService {
 
 		Category category = mapper.map(categoryDto, Category.class);
 
-		category.setIsDeleted(false);
-		category.setCreatedBy(1);
-		category.setCreatedOn(new Date());
+		if (ObjectUtils.isEmpty(category.getId())) {
+
+			category.setIsDeleted(false);
+			category.setCreatedBy(1);
+			category.setCreatedOn(new Date());
+
+		} else {
+
+			updateCategory(category);
+
+		}
 
 		Category saveCategory = categoryRepository.save(category);
 
@@ -49,6 +57,24 @@ public class CategoryServiceImpl implements CategoryService {
 		}
 
 		return true;
+	}
+
+	private void updateCategory(Category category) {
+
+		Optional<Category> findById = categoryRepository.findById(category.getId());
+
+		if (findById.isPresent()) {
+
+			Category existCategory = findById.get();
+
+			category.setCreatedBy(existCategory.getCreatedBy());
+			category.setCreatedOn(existCategory.getCreatedOn());
+			category.setIsDeleted(existCategory.getIsDeleted());
+
+			category.setUpdatedBy(1);
+			category.setUpdatedOn(new Date());
+		}
+
 	}
 
 	@Override
